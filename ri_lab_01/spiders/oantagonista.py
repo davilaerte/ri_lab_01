19f277fd119985b32b6731412f0d295a7a1d4eaa
Davi Laerte <davi.nascimento@ccc.ufcg.edu.br>
2019-04-07 04:11:31 -0300

    Finished lab

--- ri_lab_01/spiders/oantagonista.py
+++ ri_lab_01/spiders/oantagonista.py
@@ -4,12 +4,16 @@ import json
 
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
+from datetime import datetime
 
 
 class OantagonistaSpider(scrapy.Spider):
     name = 'oantagonista'
     allowed_domains = ['oantagonista.com']
     start_urls = []
+
+    data_limite = datetime.strptime('01/01/2018', '%d/%m/%Y')
+    news_number = 150
 
     def __init__(self, *a, **kw):
         super(OantagonistaSpider, self).__init__(*a, **kw)
@@ -18,14 +22,27 @@ class OantagonistaSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-        #
-        #
-        #
+        first_url = response.css('a.article_link::attr(href)').get()
+
+        yield response.follow(first_url, callback=self.parse_content)
+    
+    def parse_content(self, response):
+        data = datetime.strptime(response.css('time.published::attr(datetime)').get(), '%Y-%m-%d %H:%M:%S')
+
+        if (data > self.data_limite):
+            yield {
+                'title': response.css('h1.entry-title::text').get(),
+                'subtitle': '-',
+                'author': '-',
+                'date': data.strftime("%d/%m/%Y %H:%M:%S"),
+                'section': response.css('span.categoria a::text').get(),
+                'text': ''.join(response.css('div.entry-content p::text').getall()),
+                'url': response.url
+            }
+
+            next_url = response.css('div.nex-prev-links a::attr(href)').re_first('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+            self.news_number -= 1
+
+            if self.news_number > 0:
+                yield response.follow(next_url, callback=self.parse_content)
